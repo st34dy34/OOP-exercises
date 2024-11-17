3c410d37ca301cbf6b7e7eaa3f09b3124db7d48d
steady7 <hruskadave01@gmail.com>
2024-11-17 15:18:38 +0100

    + class Book

--- main.py
+++ main.py
@@ -19,3 +19,32 @@ class Car:
     
 car1 = Car(model="Diablo",release_year="1975",manufacturer="Lamborghini", engine_capacity= "1.5", color="Red", price = 987)
 print(car1)
+
+#2 Implement a class Book.
+class Book:
+    def __init__(self,title, release_year, publisher, genre, author, price):
+        self.title = title
+        self.release_year = release_year
+        self.publisher = publisher
+        self.genre = genre
+        self.author = author
+        self.price = price
+    def __str__(self):
+        return f"""
+    Title: {self.title}
+    Release Year: {self.release_year}
+    Publisher: {self.publisher}
+    Genre: {self.genre}
+    Author: {self.author}
+    Price: {self.price}
+    """
+
+book1 = Book(
+    "Harry Potter",
+    1997,
+    "Bloomsbury",
+    "Fantasy",
+    "J.K. Rowling",
+    20.99
+)
+print(book1)
